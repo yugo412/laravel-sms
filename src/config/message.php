16a24d1e4eaa7f17bd49b45cf9e9b209f0e6496b
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2018-05-09 13:21:53 +0700

    Fix default SMS vendor

--- src/config/message.php
+++ src/config/message.php
@@ -16,5 +16,15 @@ return [
     'smsgatewayme' => [
         'device' => env('SMSGATEWAYME_DEVICE'),
         'token' => env('SMSGATEWAYME_TOKEN'),
+    ],
+
+    /**
+     * Set configuration from Zenziva.
+     * 
+     * @link https://reguler.zenziva.net/apps/api.php
+     */
+    'zenziva' => [
+        'userkey' => env('ZENZIVA_USERKEY'),
+        'passkey' => env('ZENZIVA_PASSKEY'),
     ]
 ];
